6cf418030d6159a335b1a1a45028ec2ffc9543db
C0WN0W <imcownow@yeah.net>
2026-06-29 19:31:45 +0800

    Added OS-ImGui

---
+++ OS-ImGui/OS-ImGui_Base.cpp
@@ -0,0 +1,44 @@
+#include "OS-ImGui_Base.h"
+
+namespace OSImGui
+{
+    bool OSImGui_Base::InitImGui(ID3D11Device* device, ID3D11DeviceContext* device_context)
+    {
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+        ImGui::StyleColorsLight();
+        io.LogFilename = nullptr;
+
+        if (!ImGui_ImplWin32_Init(Window.hWnd))
+            throw OSException("ImGui_ImplWin32_Init() call failed.");
+        if (!ImGui_ImplDX11_Init(device, device_context))
+            throw OSException("ImGui_ImplDX11_Init() call failed.");
+
+        return true;
+    }
+
+    void OSImGui_Base::CleanImGui()
+    {
+        ImGui_ImplDX11_Shutdown();
+        ImGui_ImplWin32_Shutdown();
+        ImGui::DestroyContext();
+
+        if(g_Device.g_pd3dDevice)
+            g_Device.CleanupDeviceD3D();
+
+        // Only destroy window in external mode.
+        if (Window.hWnd && !Window.Name.empty())
+        {
+            DestroyWindow(Window.hWnd);
+            UnregisterClassA(Window.ClassName.c_str(), Window.hInstance);
+        }
+    }
+
+    std::wstring OSImGui_Base::StringToWstring(std::string& str)
+    {
+        std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+        return converter.from_bytes(str);
+    }
+
+}
